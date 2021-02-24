fde270e43f2b3f1458d173ed4f270070a58404bc
Cfun <15718354+Cfun1@users.noreply.github.com>
2021-02-24 02:43:26 +0100

    Rename Control

---
+++ XamsungHealth/Controls/StepsProgressView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamsungHealth.Controls
+{
+	public class StepsProgressView : ContentView
+	{
+
+		ProgressCircle ProgressCircle;
+
+
+		public StepsProgressView()
+		{
+
+		}
+
+
+
+
+
+
+	}
+}
