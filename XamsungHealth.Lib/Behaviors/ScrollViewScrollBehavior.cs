1d6c9ebc5e25122c9f70f3832f86f950b70076ee
Cfun1 <15718354+Cfun1@users.noreply.github.com>
2021-03-30 04:27:09 +0100

    - Dynamic color and Shadow of AppBar  depending on CollectionView scrolling       Added CollectionViewScrollBehavior       Added ConditionalDoubleConverter

---
+++ XamsungHealth.Lib/Behaviors/ScrollViewScrollBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamsungHealth.Lib.Behaviors
+{
+	//Adapted from https://github.com/jsuarezruiz/Xamanimation/blob/master/Xamanimation/Behaviors/ScrollViewScrollBehavior.cs
+	public class CollectionViewScrollBehavior : Behavior<CollectionView>
+	{
+		public static readonly BindableProperty ScrollXProperty =
+			BindableProperty.Create(nameof(ScrollX), typeof(double), typeof(CollectionViewScrollBehavior), default(double),
+				BindingMode.TwoWay, null);
+
+		/// <summary>
+		/// The horizontal scroll value in pixels.
+		/// </summary>
+		public double ScrollX
+		{
+			get { return (double)GetValue(ScrollXProperty); }
+			set { SetValue(ScrollXProperty, value); }
+		}
+
+		public static readonly BindableProperty ScrollYProperty =
+			BindableProperty.Create(nameof(ScrollY), typeof(double), typeof(CollectionViewScrollBehavior), default(double),
+				BindingMode.TwoWay, null);
+
+		/// <summary>
+		/// The vertical scroll value in pixels.
+		/// </summary>
+		public double ScrollY
+		{
+			get { return (double)GetValue(ScrollYProperty); }
+			set { SetValue(ScrollYProperty, value); }
+		}
+
+		protected override void OnAttachedTo(CollectionView bindable)
+		{
+			base.OnAttachedTo(bindable);
+			bindable.Scrolled += new EventHandler<ItemsViewScrolledEventArgs>(OnScrolled);
+		}
+
+		protected override void OnDetachingFrom(CollectionView bindable)
+		{
+			base.OnDetachingFrom(bindable);
+			bindable.Scrolled -= new EventHandler<ItemsViewScrolledEventArgs>(OnScrolled);
+		}
+
+		private void OnScrolled(object sender, ItemsViewScrolledEventArgs e)
+		{
+			ScrollY = e.VerticalOffset;
+			ScrollX = e.HorizontalOffset;
+
+			Console.WriteLine(ScrollY);
+			//Size contentSize = scrollView.ContentSize;
+
+			//var viewportHeight = contentSize.Height - collectionView.Height;
+			//var viewportWidth = contentSize.Width - collectionView.Width;
+
+			//RelativeScrollY = viewportHeight <= 0 ? 0 : e.VerticalOffset / viewportHeight;
+			//RelativeScrollX = viewportWidth <= 0 ? 0 : e.HorizontalOffset / viewportWidth;
+
+			//PercentageScrollX = RelativeScrollX * 100;
+			//PercentageScrollY = RelativeScrollY * 100;
+		}
+	}
+}
---
+++ XamsungHealth.Lib/Converters/ConditionalDoubleConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.CommunityToolkit.Extensions.Internals;
+using Xamarin.Forms;
+
+namespace XamsungHealth.Lib.Converters
+{
+	public class ConditionalDoubleConverter : ConditionalDoubleConverter<object>
+	{
+	}
+
+	public class ConditionalDoubleConverter<TObject> : ValueConverterExtension, IValueConverter
+	{
+		enum Modes
+		{
+			Bool,
+			Object
+
+		}
+		Modes Mode = Modes.Bool;
+
+		public double? Value { get; set; }
+		public string? Condition { get; set; }
+		public TObject? TrueObject { get; set; }
+		public TObject? FalseObject { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (Condition == null || Value == null)
+				throw new ArgumentNullException("Value and Condition parameters shouldn't be null");
+
+			if (value is not double)
+				throw new ArgumentNullException("value is expected to be of type double");
+
+			if (!(TrueObject == null ^ FalseObject != null))
+				throw new ArgumentNullException("Both TrueObject and FalseObject should be defined or omited");
+
+			if (TrueObject != null)
+				Mode = Modes.Object;
+
+			return (Condition) switch
+			{
+				"<" => EvaluateCondition((double)value < Value),
+
+				"<=" => EvaluateCondition((double)value <= Value),
+				"==" => EvaluateCondition((double)value == Value),
+				"!=" => EvaluateCondition((double)value != Value),
+				">=" => EvaluateCondition((double)value >= Value),
+				">" => EvaluateCondition((double)value > Value),
+				_ => throw new ArgumentNullException("Condition {0} Not found.", Condition),
+			};
+		}
+
+		private object EvaluateCondition(bool v)
+			=> v ? Mode == Modes.Object ?
+					TrueObject! : true
+						: Mode == Modes.Object ?
+							FalseObject! : false;
+
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+				=> throw new NotImplementedException();
+	}
+}
