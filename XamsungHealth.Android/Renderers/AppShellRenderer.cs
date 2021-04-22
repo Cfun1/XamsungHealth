4306eabd40b2b1851a6918151601596aa43135a9
Cfun1 <15718354+Cfun1@users.noreply.github.com>
2021-04-22 15:01:25 +0100

    - Change flyout icon color - Flyout translate animation

---
+++ XamsungHealth.Android/Renderers/AppShellRenderer.cs
@@ -0,0 +1,62 @@
+﻿using Android.Content;
+using AndroidX.AppCompat.Widget;
+using Google.Android.Material.AppBar;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamsungHealth.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(XamsungHealth.AppShell), typeof(AppShellRenderer))]
+namespace XamsungHealth.Droid.Renderers
+{
+	public class AppShellRenderer : ShellRenderer
+	{
+		public AppShellRenderer(Context context) : base(context)
+		{
+
+		}
+
+		protected override IShellFlyoutRenderer CreateShellFlyoutRenderer()
+		{
+			return new CustomShellFlyoutRenderer(this, AndroidContext);
+		}
+
+		protected override IShellToolbarAppearanceTracker CreateToolbarAppearanceTracker()
+		{
+			return new MyShellToolbarAppearanceTracker(this);
+		}
+		//try use MessagingCenter to change dynamically
+	}
+
+
+	internal class CustomShellFlyoutRenderer : ShellFlyoutRenderer
+	{
+		public CustomShellFlyoutRenderer(IShellContext shellContext, Context context) : base(shellContext, context)
+		{
+		}
+
+		public override void AddDrawerListener(IDrawerListener listener)
+		{
+			base.AddDrawerListener(listener);
+			DrawerSlide += CustomShellFlyoutRenderer_DrawerSlide;
+		}
+
+		void CustomShellFlyoutRenderer_DrawerSlide(object sender, DrawerSlideEventArgs e)
+		{
+			(Shell.Current as AppShell).FlyoutOpenedPercentage = e.SlideOffset;
+			(Shell.Current as AppShell).FlyoutWidth = e.DrawerView.Width / Xamarin.Essentials.DeviceDisplay.MainDisplayInfo.Density;
+		}
+	}
+
+	class MyShellToolbarAppearanceTracker : ShellToolbarAppearanceTracker
+	{
+		public MyShellToolbarAppearanceTracker(IShellContext context) : base(context)
+		{
+		}
+
+		public override void SetAppearance(Toolbar toolbar, IShellToolbarTracker toolbarTracker, ShellAppearance appearance)
+		{
+			base.SetAppearance(toolbar, toolbarTracker, appearance);
+			toolbarTracker.TintColor = Color.Black;
+		}
+	}
+}
--- XamsungHealth/AppShell.xaml.cs
+++ XamsungHealth/AppShell.xaml.cs
@@ -1,19 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Runtime.CompilerServices;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace XamsungHealth
 {
 	public partial class AppShell : Shell
 	{
+		float flyoutOpenedPercentage;
+		public float FlyoutOpenedPercentage
+		{
+			get => flyoutOpenedPercentage;
+			set
+			{
+				flyoutOpenedPercentage = value;
+				OnPropertyChanged(nameof(FlyoutOpenedPercentage));
+			}
+		}
+
 		public AppShell()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+		{
+			base.OnPropertyChanged(propertyName);
+			if (propertyName.Equals(nameof(FlyoutOpenedPercentage)))
+			{
+				if (Current.CurrentPage != null)
+				{
+					Current.CurrentPage.TranslationX = Current.FlyoutWidth * FlyoutOpenedPercentage;
+
+					if (Current.CurrentPage.GetValue(TitleViewProperty) is ContentView titleView)
+					{
+						titleView.TranslationX = Current.FlyoutWidth * FlyoutOpenedPercentage;
+					}
+				}
+			}
 		}
 	}
 }
